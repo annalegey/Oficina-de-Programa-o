409bb729d42a2c5fe29b1e648263eeaeacbf006a
annalegey <133767348+annalegey@users.noreply.github.com>
2023-05-29 14:45:44 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -1,4 +1,7 @@
 import csv
+import pandas as pd
 arquivo = open('brasil-win.csv', encoding = 'ISO-8859-1')
 for registro in csv.reader(arquivo,delimiter =';'):
   print(registro)
+  df = pd.DataFrame(registro)
+  df
