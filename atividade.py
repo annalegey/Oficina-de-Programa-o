cb9d8d759e0faba2e97154dc26fcb9920de4d138
annalegey <133767348+annalegey@users.noreply.github.com>
2023-05-29 08:44:17 -0300

    atividade.py

--- atividade.py
+++ atividade.py
@@ -1,7 +1,8 @@
 import csv
+python manage.py import_data --no-input brasil-win.csv.xz
 arquivo = open('brasil-win.csv', encoding = 'ISO-8859-1')
 contador = 0
 for registro in csv.reader(arquivo,delimiter =';'):
   print(registro)
-  if contador == 11:
+  if contador == 10:
  
