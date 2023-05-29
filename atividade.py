a2268d4729bb35f012b4b9a8f4dc35f6f594810a
annalegey <133767348+annalegey@users.noreply.github.com>
2023-05-29 08:47:03 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -1,5 +1,4 @@
 import csv
-python manage.py import_data --no-input brasil-win.csv.xz
 arquivo = open('brasil-win.csv', encoding = 'ISO-8859-1')
 contador = 0
 for registro in csv.reader(arquivo,delimiter =';'):
