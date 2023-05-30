19851a9dffd9855c03d4e3c0fff3f5cdbe3480d8
annalegey <133767348+annalegey@users.noreply.github.com>
2023-05-30 09:03:49 -0300

    Create grafico.py

---
+++ grafico.py
@@ -0,0 +1,9 @@
+import csv
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+arquivo = open('brasil-win.csv', encoding = 'ISO-8859-1')
+for registro in csv.reader(arquivo,delimiter =','):
+    st.write(registro)
+    st.bar_chart(registro)
