8e1313f13fd3fe6fc3c338b930a222ff02c982aa
annalegey <133767348+annalegey@users.noreply.github.com>
2023-05-30 08:38:24 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -1,4 +1,9 @@
-import csv
+import csv import streamlit as st
+import pandas as pd
+import numpy as np
+
 arquivo = open('brasil-win.csv', encoding = 'ISO-8859-1')
 for registro in csv.reader(arquivo,delimiter =','):
     
+st.title('Municípios')
+st.write("Tabela")
