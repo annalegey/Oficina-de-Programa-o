64ba1d4d8ae3369ca8a992c52157757095bad7ef
annalegey <133767348+annalegey@users.noreply.github.com>
2023-06-02 08:01:28 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -1,7 +1,8 @@
+import csv
 import streamlit as st
 import pandas as pd
 import numpy as np
 
-pd.read_csv('brasil-win.csv')
-st.write(registro)
-st.area_chart(registro)
+df = pd.read_csv('brasil-win.csv')
+st.write(df)
+st.area_chart(df)
