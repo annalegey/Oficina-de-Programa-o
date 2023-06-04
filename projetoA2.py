8f4789762292c7930a3cf956f1c5348fff3deda7
annalegey <133767348+annalegey@users.noreply.github.com>
2023-06-04 16:10:07 -0300

    Update projetoA2.py

--- projetoA2.py
+++ projetoA2.py
@@ -4,16 +4,8 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 st.title("Consulta de Preços de Livros")
-st.markdown(
-    """
-    <style>
-    body {
-        background-color: lightblue;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+st.markdown("<span style='color: red;'>Texto em vermelho</span>", unsafe_allow_html=True)
+
 
 genero = st.text_input("Selecione o gênero de livro que você deseja:")
 genero = genero.replace(' ', '-')
