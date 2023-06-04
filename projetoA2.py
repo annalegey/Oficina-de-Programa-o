cadb084471e20c934b6fddd73a3153a2f97c31e5
annalegey <133767348+annalegey@users.noreply.github.com>
2023-06-04 15:32:44 -0300

    Update projetoA2.py

--- projetoA2.py
+++ projetoA2.py
@@ -1,34 +1,40 @@
+import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-genêro = st.text_input("Selecione o genêro de livro que você deseja:").replace(' ','-')
-url = 'https://leitura.com.br/livros/' + genêro
-site = requests.get('https://leitura.com.br/livros/' + genêro)
-soup = BeautifulSoup(site.content, 'html.parser')
+st.title("Consulta de Preços de Livros")
 
-import requests
-from bs4 import BeautifulSoup
+genero = st.text_input("Selecione o gênero de livro que você deseja:")
+genero = genero.replace(' ', '-')
 
-lista = []
-b = soup.find('div', {'id': 'content'})
-livros = b.find_all('div', {'class': 'product-layout'})
-for links in livros:
-    links_p = links.find('div', 'image')
-    link_livro = links_p.find('a')['href']
-    conteúdo_site = links.find_all('div', {'class': 'caption'})
-    for nome in conteúdo_site:
-        nome_livro = nome.find('h4').text
-        preços = nome.find_all('p', {'class': 'price'})
-        for preço in preços:
-            preços_new = preço.find('span', {'class': 'price-new'})
-            if preços_new:
-               preços_new = preço.find('span', {'class': 'price-new'}).text
-            else:
-                preços_new = preço.text.strip()
+if st.button("Consultar"):
+    url = 'https://leitura.com.br/livros/' + genero
+    site = requests.get(url)
+    soup = BeautifulSoup(site.content, 'html.parser')
 
-            dados = {'Livro': nome_livro, 'Preço do livro': preços_new, 'Link para compra' : link_livro}
-            lista.append(dados)
+    lista = []
+    b = soup.find('div', {'id': 'content'})
+    livros = b.find_all('div', {'class': 'product-layout'})
+    for links in livros:
+        links_p = links.find('div', 'image')
+        link_livro = links_p.find('a')['href']
+        conteudo_site = links.find_all('div', {'class': 'caption'})
+        for nome in conteudo_site:
+            nome_livro = nome.find('h4').text
+            precos = nome.find_all('p', {'class': 'price'})
+            for preco in precos:
+                preco_new = preco.find('span', {'class': 'price-new'})
+                if preco_new:
+                    preco_new = preco.find('span', {'class': 'price-new'}).text
+                else:
+                    preco_new = preco.text.strip()
 
-df = pd.DataFrame(lista).sort_values(by='Preço do livro', ascending=True)
-df
+                dados = {'Livro': nome_livro, 'Preço do livro': preco_new, 'Link para compra': link_livro}
+                lista.append(dados)
+
+    if lista:
+        df = pd.DataFrame(lista).sort_values(by='Preço do livro', ascending=True)
+        st.table(df)
+    else:
+        st.write("Nenhum livro encontrado")
